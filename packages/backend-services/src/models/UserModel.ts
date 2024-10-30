4bae9abfe42a059d8d5b80d987bcac96607c930a
Kamil Błoński <kblonski02@gmail.com>
2024-10-30 16:51:16 +0100

    Create UserModel with UserType

--- packages/backend-services/src/models/UserModel.ts
+++ packages/backend-services/src/models/UserModel.ts
@@ -1,7 +1,46 @@
+import { sequelize } from "../utils/db";
+import { DataTypes, InferAttributes, InferCreationAttributes, Model } from "sequelize";
+import UserType from "../types/UserType";
 
-// Tabela users
-// id: unikalny identyfikator użytkownika
-// username: nazwa użytkownika (unikalna)
-// email: adres e-mail (unikalny)
-// password: hasło (szyfrowane)
-// created_at: data utworzenia konta
+
+export class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> implements UserType{
+    declare id: number;
+    declare username: string;
+    declare email: string;
+    declare password: string;
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            validate: {
+                isInt: true
+            }
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // unique: true - // comment for development
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: { msg: "Password is needed"},
+                notEmpty: { msg: "Please provide a password"}
+            }
+
+        }
+    }, {
+        sequelize,
+        timestamps: false
+    }
+)
