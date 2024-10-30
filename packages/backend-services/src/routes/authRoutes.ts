68807e4435f6c33925560b7d4f7277177b32d4e0
Kamil Błoński <kblonski02@gmail.com>
2024-10-30 23:09:12 +0100

    Add api register

--- packages/backend-services/src/routes/authRoutes.ts
+++ packages/backend-services/src/routes/authRoutes.ts
@@ -6,14 +6,28 @@ import { userController } from "../controllers/controllers";
 
 const router = Router();
 
-router.post(buildApiPath("login"), async (req, res) => {
-    const { email, password }: Partial<UserType> = req.body;
-    if (!email || !password) return sendErrorResponse(res, 400);
-    const userDb = await userController.getByEmail(email);
-    if (!userDb) return sendErrorResponse(res, 404);
-    if (!(await userController.validPassword(password, userDb)))
-        return sendErrorResponse(res, 400);
-    sendSuccessResponse( res, {data: 'success'})
+router.post(
+    buildApiPath('register'),
+    async (req, res) => {
+        const { username, email, password } : UserType = req.body
+        if (!username || !email || !password) return sendErrorResponse( res, 400);
+        const userExist = await userController.getByEmail( email )
+        if (userExist) return sendErrorResponse( res, 403 )
+        const newUser = await userController.create( {email, password, username});
+        return sendSuccessResponse( res, { token: newUser}, 200)
+    }
+)
+
+router.post(
+    buildApiPath("login"),
+    async (req, res) => {
+        const { email, password }: Partial<UserType> = req.body;
+        if (!email || !password) return sendErrorResponse(res, 400);
+        const userDb = await userController.getByEmail(email);
+        if (!userDb) return sendErrorResponse(res, 404);
+        if (!(await userController.validPassword(password, userDb)))
+            return sendErrorResponse(res, 400);
+        sendSuccessResponse( res, {data: 'success'})
 });
 
 export default router;
