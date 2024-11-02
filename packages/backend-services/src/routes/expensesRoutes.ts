df48ee68550df0efc68413c28b73726ab96056c7
Kamil Błoński <kblonski02@gmail.com>
2024-11-02 06:22:19 +0100

    Add /api/expneses/:id patch - update

--- packages/backend-services/src/routes/expensesRoutes.ts
+++ packages/backend-services/src/routes/expensesRoutes.ts
@@ -88,7 +88,42 @@ router.get(
             const accessToExpense = await expenseController.isAccessForUser(expenseDb,  req.user);
             if (!accessToExpense) return sendErrorResponse(res, 403);
 
+            console.error('err')
             return sendSuccessResponse(res, 200, { expense: expenseDb})
+        } catch(err){
+            return sendErrorResponse(res, 500)
+        }
+    }
+);
+
+router.patch(
+    buildApiPath("expenses", ":id"),
+    async (req, res) =>{
+        try {
+            if (!req.user) return sendErrorResponse(res, 401);
+            const {id} = req.params;
+            const { amount, description, user_id, category_id  } : Partial<ExpenseType> = req.body;
+            if (
+                !isNumber(id) 
+            ) return sendErrorResponse(res, 400, "Invalid type id, id must be a number");
+            if (
+                !amount && !description && !user_id && !category_id ||
+                amount && !isNumber(amount) ||
+                user_id && !isNumber(user_id) ||
+                category_id && !isNumber(category_id)
+            ) return sendErrorResponse(res, 400, "Please provide amount, user_id or category_id");
+
+            const expenseDb = await expenseController.getById(Number(id));
+            if (!expenseDb) return sendErrorResponse(res, 404);
+
+            const accessToExpense = await expenseController.isAccessForUser(expenseDb,  req.user);
+            if (!accessToExpense) return sendErrorResponse(res, 403);
+
+            await expenseController.updateById( expenseDb.id, {
+                amount, description, user_id, category_id
+            })
+
+            return sendSuccessResponse(res, 204)
         } catch(err){
             return sendErrorResponse(res, 500)
         }
