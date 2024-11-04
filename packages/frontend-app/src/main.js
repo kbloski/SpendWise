4740a5af29ed72147e4792ede6e95dbb0ecdebb6
Kamil Błoński <kblonski02@gmail.com>
2024-11-04 07:14:15 +0100

    Init store

--- packages/frontend-app/src/main.js
+++ packages/frontend-app/src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index.js'
+import store from './store/index.js'
 import BaseCard from './components/UI/BaseCard.vue'
-
 const app = createApp(App)
 
 app.component('base-card', BaseCard)
-app.use(router)
+app.use( router )
+app.use( store )
 
 app.mount('#app')
