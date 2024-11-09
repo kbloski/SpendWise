4d47f375efa80dfff9ed1f4e65ef02a26927e685
Kamil Błoński <kblonski02@gmail.com>
2024-11-09 09:51:21 +0100

    Add simple report page

--- packages/frontend-app/src/pages/Reports.vue
+++ packages/frontend-app/src/pages/Reports.vue
@@ -1,3 +1,43 @@
 <template>
-    <side-card>Reports</side-card>
+    <div>
+        <side-title>All Reports</side-title>
+        <ul>
+            <li
+                v-for="report in reports"
+                :key="report.id"
+            >
+                <div>
+                    Budget: {{  report.budget_id }}
+                </div>
+                <div>
+               Total Expenses: {{  report.total_expenses }}
+                </div>
+                <div>
+               Start: {{  report.period_start }}
+                </div>
+                <div>
+               End:  {{  report.period_end }}
+                </div>
+                <div>
+               Created:  {{  report.createdAt }}
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script>
+import { computed } from 'vue';
+import useFetch from '../hooks/useFetch';
+
+export default {
+    setup(){
+        const fetchReports = useFetch('/api/reports/all');
+        const reports = computed( () => fetchReports.data.value?.reports)
+
+        return {
+            reports
+        }
+    }
+}
+</script>
