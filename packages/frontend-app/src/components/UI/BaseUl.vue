a5e5a63d090fa67aabaca2e895da5081da11169c
Kamil Błoński <kblonski02@gmail.com>
2024-11-11 13:16:17 +0100

    Add base ul

---
+++ packages/frontend-app/src/components/UI/BaseUl.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <slot></slot>
+    </ul>
+</template>
+
+<style scoped>
+ul {
+    background-color: white;
+    box-shadow: inset 0 0 4px black;
+    list-style: none;
+    padding: 1rem;
+    margin: 1rem 0;
+    max-height: 150px;
+    overflow-y: auto;
+    direction: rtl;
+}
+</style>
