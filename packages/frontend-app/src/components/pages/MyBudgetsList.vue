8fb46bb32dc0ef466daad39cf89e436b1a218499
Kamil Błoński <kblonski02@gmail.com>
2024-11-14 11:07:30 +0100

    Add search budgets

--- packages/frontend-app/src/components/pages/MyBudgetsList.vue
+++ packages/frontend-app/src/components/pages/MyBudgetsList.vue
@@ -3,6 +3,28 @@
         <base-error v-if="errorMessage">{{  errorMessage }}</base-error>
         <div v-if="loading" >Loading... </div>
 
+        <div class="sort-options">
+            <base-form-control v-model="searchName">Szukaj budżetu po nazwie.</base-form-control>
+        </div>
+
+        <transition name="search-fade" mode="out-in">
+            <div v-if="searchName">
+                <base-ul title="Znalezione budżety" v-if="searchName">
+                    <li v-if="!findedBudgets?.length">Nie dopasowano żadnego budżetu do wyszukiwań.</li>
+                    <my-budgets-list-item  
+                        v-else
+                        v-for="rel in findedBudgets"
+                        :key="rel.Budget.id"
+                        :id="rel.Budget.id"
+                        :name="rel.Budget.name"
+                        :rolePriority="rel.role"
+                    >
+                    </my-budgets-list-item>
+                </base-ul>
+            </div>
+        </transition>
+
+
         <div v-if="budgetList.length">
             <base-ul title="Moje budżety">
                 <my-budgets-list-item 
@@ -20,7 +42,7 @@
 </template>
 
 <script>
-import { computed, provide, watch } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { useStore } from 'vuex';
 import useFetch from '../../hooks/useFetch';
 import MyBudgetsListItem from './MyBudgetsListItem.vue';
@@ -31,7 +53,6 @@ export default {
     },
     setup(){
         const store = useStore()
-        
         const isNeededRefresh = computed(() => store.getters['refresh/isRefreshBudgetsNeeded'] );
         watch( isNeededRefresh, () => {
             if (!isNeededRefresh.value) return;
@@ -42,14 +63,53 @@ export default {
         const errorMessage = computed( () => fetchBudgets?.error.value);
         const loading = computed( ()=> fetchBudgets.loading.value );
         const budgetList = computed( () => fetchBudgets?.data?.value?.budgets ?? []);
-        
+
+        const searchName = ref(null);
+        const findedBudgets = ref([]);
+        watch( searchName, () => {
+            findedBudgets.value =  budgetList.value.filter( rel => rel.Budget.name.includes( searchName.value ))
+        })
 
         return {
             errorMessage,
             loading: loading,
             budgetList,
+            searchName,
+            findedBudgets
         }
     }
 }
 </script>
 
+<style lang="css" scoped>
+.sort-options {
+    /* display: flex; */
+    padding: 2rem;
+}
+
+
+.search-fade-enter-from {
+    transform: translateY(-50px);
+    scale: 0.8;
+    opacity: 0;
+}
+.search-fade-enter-active {
+    transition: all 0.3s ease-in;
+}
+.search-fade-enter-to {
+    transform: translateY(0);
+    scale: 100%;
+    opacity: 1;
+}
+.search-fade-leave-from {
+}
+
+.search-fade-leave-active {
+    transition: all 0.3s ease-out;
+}
+.search-fade-leave-to {
+    transform: translateY(-50px);
+    scale: 0.8;
+    opacity: 0;
+}
+</style>
